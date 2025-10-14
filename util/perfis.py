a250c9569ae82eb475a586f46d940b56b2eef980
Lavinia Paris <parislavinia13@gmail.com>
2025-10-14 20:56:40 -0300

    Novos perfis

--- util/perfis.py
+++ util/perfis.py
@@ -17,7 +17,8 @@ class Perfil(str, Enum):
 
     # PERFIS DO SEU SISTEMA #####################################
     ADMIN = "Administrador"
-    CLIENTE = "Cliente"
+    ABRIGO = "Abrigo"
+    ADOTANTE = "Adotante"
     # FIM DOS PERFIS ############################################
 
     def __str__(self) -> str:
